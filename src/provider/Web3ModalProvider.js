0fe05d5f265a1919296bcfd3266f747b7baf36a1
Angel Gonzalez <aangel.gg@hotmail.com>
2024-04-01 21:09:05 +0200

    Customize Web3Modal

--- src/provider/Web3ModalProvider.js
+++ src/provider/Web3ModalProvider.js
@@ -4,7 +4,7 @@ import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { mainnet, polygon, polygonMumbai } from "viem/chains";
 import { WagmiProvider } from "wagmi";
 
-const projectId = "";
+const projectId = process.env.REACT_APP_PROJECT_ID;
 
 const metadata = {
   name: "Tutorial Web3Modal",
@@ -18,11 +18,32 @@ const config = defaultWagmiConfig({
   chains,
   projectId,
   metadata,
+  enableEmail: true,
 });
 
 createWeb3Modal({
   wagmiConfig: config,
   projectId,
+  defaultChain: mainnet,
+  featuredWalletIds: [
+    "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",
+    "c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a",
+    "74f8092562bd79675e276d8b2062a83601a4106d30202f2d509195e30e19673d",
+  ],
+  excludeWalletIds: [
+    "225affb176778569276e484e1b92637ad061b01e13a048b35a9d280c3b58970f",
+    "ecc4036f814562b41a5268adc86270fba1365471402006302e70169465b7ac18",
+  ],
+  termsConditionsUrl: "https://www.conquerx.com/terminos-y-condiciones",
+  privacyPolicyUrl: "https://www.conquerx.com/politica-de-privacidad",
+  enableAnalytics: true,
+  allWallets: "HIDE",
+  enableOnramp: true,
+  themeVariables: {
+    "--w3m-color-mix": "#7f56d9",
+    "--w3m-color-mix-strength": 40,
+    "--w3m-accent": "#ff8a56",
+  },
 });
 
 const queryClient = new QueryClient();
